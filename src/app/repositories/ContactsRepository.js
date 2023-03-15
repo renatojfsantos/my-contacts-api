7589ceb14f1f2d75a0b50a40fa7b371bca1c9e81
Renato Santos <renato_jfs@hotmail.com>
2023-03-15 00:23:59 -0300

    Creating the show and delete routes

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,12 +1,19 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'Renato',
     email: 'renato@mail.com',
     phone: '123456789',
-    category_id: uuid(),
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'José',
+    email: 'jose@mail.com',
+    phone: '1236675489',
+    category_id: v4(),
   },
 ];
 
@@ -14,6 +21,20 @@ class ContactsRepository {
   findAll() {
     return new Promise((resolve) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+
+      resolve();
     });
   }
 }
