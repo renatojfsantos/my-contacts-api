c8020d2186cb767ef2ec95e02fbc2afcd7320d1a
Renato Santos <renato_jfs@hotmail.com>
2023-03-16 22:30:38 -0300

    Creating the Contacts registration route

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -5,12 +5,9 @@ class ContactController {
     const contacts = await ContactsRepository.findAll();
 
     response.json(contacts);
-    // retornar middleware
-    // response.send(request.appId);
   }
 
   async show(request, response) {
-    // Obter um registro
     const { id } = request.params;
 
     const contact = await ContactsRepository.findById(id);
@@ -22,8 +19,26 @@ class ContactController {
     response.json(contact);
   }
 
-  store() {
-    // Criar novo registro
+  async store(request, response) {
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if (contactExists) {
+      return response.status(400).json({ error: 'This email is already been taken' });
+    }
+
+    const contact = await ContactsRepository.create({
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   update() {
@@ -31,21 +46,17 @@ class ContactController {
   }
 
   async delete(request, response) {
-    // Deletar um registro
     const { id } = request.params;
 
     const contact = await ContactsRepository.findById(id);
 
     if (!contact) {
-      // 404: Not Found
       return response.status(404).json({ error: 'User not found' });
     }
 
     await ContactsRepository.delete(id);
-    // 204: No Content
     response.sendStatus(204);
   }
 }
 
-// Singleton
 module.exports = new ContactController();
